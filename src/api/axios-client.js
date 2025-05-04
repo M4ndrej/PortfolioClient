e92805deeb6c0ebda4c0cc4925edb764441cceb2
M4ndrej <andrejmax347@gmail.com>
2025-05-04 17:04:38 +0200

    baseURl

--- src/api/axios-client.js
+++ src/api/axios-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/', 
+    baseURL: 'https://portfolioserver-production-aee2.up.railway.app/api/', 
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
   });
