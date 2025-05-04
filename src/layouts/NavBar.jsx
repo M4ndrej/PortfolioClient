2ff3a648176e918538270e1816dbd0858a0907ac
M4ndrej <andrejmax347@gmail.com>
2025-05-04 17:43:40 +0200

    redirects added

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,46 +1,50 @@
-import { Fragment, useState, useEffect } from "react";
-export default function NavBar() {
+import { useState } from "react";
 
+export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  // Toggle open and closed state for the menu
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {}, []);
+  // Close the menu when a link is clicked
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <nav className=" w-75 mx-auto d-flex justify-content-between align-items-center">
+    <nav className="w-75 mx-auto d-flex justify-content-between align-items-center">
       <div className="navBar">
         <span className={`navName ${isOpen ? "show" : ""}`}>
           Andrej Maksimovic
         </span>
         <ul className={`navLink ${isOpen ? "show" : ""}`}>
           <li>
-            <a href="/#about" className="navItem">
+            <a href="/#about" className="navItem" onClick={closeMenu}>
               ABOUT
             </a>
           </li>
           <li>
-          <a href="/#experience" className="navItem">
-          EXPERIENCE
+            <a href="/#experience" className="navItem" onClick={closeMenu}>
+              EXPERIENCE
             </a>
           </li>
           <li>
-          <a href="/#projects" className="navItem">
-          PROJECTS
+            <a href="/#projects" className="navItem" onClick={closeMenu}>
+              PROJECTS
             </a>
           </li>
           <li>
-          <a href="/#contact" className="navItem">
-          CONTACT
+            <a href="/#contact" className="navItem" onClick={closeMenu}>
+              CONTACT
             </a>
           </li>
         </ul>
       </div>
       <div
         className={`burgerMenu ${isOpen ? "open" : ""}`}
-        onClick={toggleOpen}
+        onClick={toggleOpen} // Call toggleOpen function to toggle the state
       >
         <span></span>
         <span></span>
